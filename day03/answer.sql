ee2f637a4ca5a1fa92535637d274f2e9de5282aa
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2021-12-03 09:51:02 -0800

    day03 part 1

---
+++ day03/answer.sql
@@ -0,0 +1,35 @@
+-- Put everything in a schema so its easy to reset. This also makes
+-- this automatically reset so we can just run it many times.
+DROP SCHEMA IF EXISTS day03 CASCADE;
+CREATE SCHEMA day03;
+
+CREATE TABLE day03.inputs (
+  id        SERIAL,
+  value     TEXT NOT NULL
+);
+
+-- Use \COPY rather than COPY so its client-side in psql
+\COPY day03.inputs (value) FROM 'day03/input.txt';
+
+-- characters: split string into one row for each character
+--
+WITH characters AS (
+  SELECT  id,
+          char,
+          index
+  FROM    day03.inputs,
+          LATERAL unnest(regexp_split_to_array(value, ''))
+             WITH ORDINALITY
+             AS   t(char, index)
+), common AS (
+  SELECT   c.index, mode() WITHIN GROUP (ORDER BY c.char) as char
+  FROM     characters c
+  GROUP BY c.index
+), result AS (
+  SELECT string_agg(char, '') as string,
+         lpad(string_agg(char, ''), 32, '0')::bit(32)::int as gamma,
+         (~ lpad(string_agg(char, ''), 32, '1')::bit(32))::int as epsilon
+  FROM   common
+)
+SELECT gamma, epsilon, gamma * epsilon as answer
+FROM   result;
